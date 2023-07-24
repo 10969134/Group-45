af19da037a6a51671b8ddfd782911d0898aec18e
OSEI VERA TWUMASI <103066486+10969134@users.noreply.github.com>
2023-07-24 19:59:06 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import * as React from 'react';
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-//import HomeScreen from "./src/Home";
-//import SignIn from "./src/SignIn";
-//import SignUp from "./src/SignUp";
-//import Setup from "./src/SetUp";
+// import { NavigationContainer } from "@react-navigation/native";
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import Onboarding from "./src/components/screen";
+import Screen0 from './src/onBoarding/Screen0';
+import Screen1 from './src/onBoarding/Screen1';
 import Screen2 from "./src/onBoarding/Screen2";
 import Screen3 from "./src/onBoarding/Screen3";
+import Login from './src/components/Screens/Login';
 
 const Stack = createNativeStackNavigator();
 const App = () => {
@@ -19,12 +21,26 @@ const App = () => {
       >
         <Stack.Screen name="OnBoarding1" component={Onboarding} />
         <Stack.Screen
-            name="OnBoarding2"
+            name="Screen2"
             component={Screen2}
           />
+
+<Stack.Screen
+            name="Screen0"
+            component={Screen0}
+          />
           <Stack.Screen
-            name="OnBoarding3"
+            name="Screen1"
+            component={Screen1}
+          />
+          <Stack.Screen
+            name="Screen3"
             component={Screen3}
+          />
+
+<Stack.Screen
+            name="Login"
+            component={Login}
           />
          {/* <Stack.Screen
             name="SignUp"
